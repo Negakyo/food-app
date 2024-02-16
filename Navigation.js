f903b52e749d6a51ab0105ae3548ee2d9ad9db1e
Negakyo <thomas.crqr@gmail.com>
2024-02-16 20:49:50 +0100

    initial commit

---
+++ Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+export default function Navigation() {
+
+    const Stack = createNativeStackNavigator();
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
